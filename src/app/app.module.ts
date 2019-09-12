5bce5a4ad7b98baf754a3ad6e69ec7a2e13385c3
robbie106gti <rleeuwerink@gmail.com>
2019-09-12 09:31:51 -0700

    added custom serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers';
+import { reducers, metaReducers, CustomSerializer } from './store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './store/effects/app.effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
 import { DoorComponent } from './pages/door/door.component';
 import { MatComponent } from './pages/mat/mat.component';
 
@@ -28,14 +28,14 @@ import { MatComponent } from './pages/mat/mat.component';
         strictStateImmutability: true,
         strictActionImmutability: true,
         strictStateSerializability: true,
-        strictActionSerializability: true,
+        strictActionSerializability: false,
       }
     }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     EffectsModule.forRoot([AppEffects]),
     StoreRouterConnectingModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -5,6 +5,10 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 import * as fromRouter from '@ngrx/router-store';
+import { RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { RouterStateSerializer } from '@ngrx/router-store';
+import { RouterState } from 'src/app/models/router';
 import { UiState } from './ui.reducer';
 import * as fromUi from './ui.reducer';
 
@@ -23,3 +27,18 @@ export const reducers: ActionReducerMap<State> = {
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
 export const getUiFeature = createFeatureSelector<fromUi.UiState>('uiState');
 
+@Injectable()
+export class CustomSerializer implements RouterStateSerializer<RouterState> {
+  serialize(routerState: RouterStateSnapshot): RouterState {
+    // console.log({ routerState });
+    const url = routerState.url;
+    const queryParams = routerState.root.queryParams;
+    let route = routerState.root;
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    const params = route.params;
+    const state = { url, params, queryParams };
+    return state;
+  }
+}
